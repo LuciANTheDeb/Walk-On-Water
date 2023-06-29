c6556e626b811046f3194ae0fbe13ae5de63c4bb
LuciANTheDeb <135169734+LuciANTheDeb@users.noreply.github.com>
2023-06-29 02:43:00 -0400

    Add files via upload

---
+++ Water Source/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.lucian.gorillatag.walkonwater";
+		public const string Name = "Walk On Water";
+		public const string Version = "1.0.0";
+	}
+}
